2e22af979b333b750d79a408ebc30bf014b98f9e
NaN_dev <52248252+NaN2005@users.noreply.github.com>
2020-12-29 20:48:16 +0100

    Delete randomUserAgent.js

--- src/utils/randomUserAgent.js
+++
@@ -1,7 +0,0 @@
-const fakeUserAgent = require('../../botnet/useragents.json');
-
-module.exports = randomUA = () => {
-    const randomIn = (array) => array[Math.floor(Math.random() * array.length)];
-    const optionsLength = randomIn(fakeUserAgent);
-    return randomIn(randomIn(optionsLength.list).useragents);
-};
